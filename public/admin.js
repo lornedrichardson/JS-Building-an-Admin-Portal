a108e7bf4e9ab7022ab116a5ff4370160cc38e98
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-05-18 22:38:09 -0700

    assignment complete plus first bonus

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function main() {
 
-// Your Code Here
+  let response = await fetch('http://localhost:3001/listBooks');
+  let books = await response.json();
+
+  books.forEach(renderBook);
+}
+
+function renderBook(book) {
+  let title = document.createElement('li');
+  title.textContent = book.title;
+
+  let amount = document.createElement('input');
+  amount.value = book.quantity;
+  
+  let updateIsBetterInSQL = document.createElement('button');
+  updateIsBetterInSQL.textContent = 'Save';
+  updateIsBetterInSQL.addEventListener('click', () => {
+    fetch('http://localhost:3001/updateBook', {
+      method: 'PATCH',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        id: book.id,
+        quantity: amount.value
+      })
+    });
+  });
+
+  let deleteIsDefinitelyBetterInSQL = document.createElement('button');
+  deleteIsDefinitelyBetterInSQL.textContent = 'Delete';
+  deleteIsDefinitelyBetterInSQL.addEventListener('click', () => fetch(`http://localhost:3001/removeBook/${book.id}`, { method: 'DELETE' }));
+
+  title.append(amount, updateIsBetterInSQL, deleteIsDefinitelyBetterInSQL);
+  document.querySelector('#book-list').append(title);
+}
+
+main();
